daeb25745cbe5f01ce193332149d283a5c0854c7
Sam Chotzen <schotzen23@cmc.edu>
2023-03-07 19:42:19 -0800

    Committing final files

--- src/visualize.py
+++ src/visualize.py
@@ -14,8 +14,14 @@ import json
 from collections import Counter,defaultdict
 import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-matplotlib.rcParams['font.family'] = ['./download?family=Noto Sans KR', 'sans-serif']
+from matplotlib.font_manager import FontProperties
+
+# add Korean font path
+font_paths = ['NotoSansKR-Regular.otf']
+
+properties = []
+for path in font_paths:
+    properties.append(FontProperties(fname=path))
 
 # open the input path
 with open(args.input_path) as f:
@@ -38,14 +44,29 @@ key, value = zip(*lists)
 # create bar graph
 plt.bar(key, value, color = 'maroon', width = 0.4)
 
-if args.input_path == 'reduced.lang':
+# check language of key
+if args.key == '#coronavirus':
+    language = 'English'
+else:
+    language = 'Korean'
+
+# add chart elements based on input path and key
+if args.input_path == 'reduced.lang' and args.key == '#coronavirus':
     plt.xlabel("Language")
     plt.ylabel("Usage level of " + args.key)
     plt.title("Tweets with " + args.key + " in each language in 2020")
-else:
+elif args.input_path == 'reduced.lang' and args.key == '#코로나바이러스':
+    plt.xlabel("Language")
+    plt.ylabel("Usage level of " + args.key, fontproperties=properties[0])
+    plt.title("Tweets with " + args.key + " in each language in 2020", fontproperties=properties[0])
+elif args.input_path == 'reduced.country' and args.key == '#coronavirus':
     plt.xlabel("Country")
     plt.ylabel("Usage level of " + args.key)
-    plt.title("Tweets with " + args.key + " from each country in 2020")
+    plt.title("Tweets with " + args.key + " in each country in 2020")
+else:
+    plt.xlabel("Country")
+    plt.ylabel("Usage level of " + args.key, fontproperties=properties[0])
+    plt.title("Tweets with " + args.key + " from each country in 2020", fontproperties=properties[0])
 
 # save bar graph file to plots folder
-plt.savefig(args.input_path + args.key + 'ba.png')
+plt.savefig(args.input_path + args.key + '.png')
